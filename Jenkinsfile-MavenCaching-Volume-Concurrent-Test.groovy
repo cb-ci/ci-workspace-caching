a2de9bc815a5dd707e2c16345d412079bca4d3b7
andreas <caternberg@gmx.de>
2023-11-22 17:57:41 +0100

    reformat

--- Jenkinsfile-MavenCaching-Volume-Concurrent-Test.groovy
+++ Jenkinsfile-MavenCaching-Volume-Concurrent-Test.groovy
@@ -1,25 +1,25 @@
 def gitMavenRepo = "https://github.com/org-folderscan-example/spring-boot-demo.git"
 def pvcMountCacheDir = "/tmp/cache/.m2"
-def k8sYaml ="""
-    apiVersion: v1
-    kind: Pod
-    spec:
-      containers:
-      - name: maven
-        image: maven:3.9.5-eclipse-temurin-17-alpine
-        command:
-        - sleep
-        args:
-        - infinity
-#        securityContext:
-#          runAsUser: 1000
-        volumeMounts:
-          - name: maven-cache
-            mountPath: ${pvcMountCacheDir}
-      volumes:
-        - name: maven-cache
-          persistentVolumeClaim:
-            claimName: maven-repo
+def k8sYaml = """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: maven
+    image: maven:3.9.5-eclipse-temurin-17-alpine
+    command:
+    - sleep
+    args:
+    - infinity
+    #securityContext:
+    #  runAsUser: 1000
+    volumeMounts:
+      - name: maven-cache
+        mountPath: ${pvcMountCacheDir}
+  volumes:
+    - name: maven-cache
+      persistentVolumeClaim:
+        claimName: maven-repo
     """
 pipeline {
     agent {
@@ -29,8 +29,8 @@ pipeline {
         }
     }
     stages {
-        stage ("clean") {
-            steps{
+        stage("clean") {
+            steps {
                 //Clean the cache so we can see concurrent access when it gets refilled
                 echo "clean before"
                 sh "rm -Rfv ${pvcMountCacheDir}/*"
@@ -42,17 +42,17 @@ pipeline {
                     axis {
                         name 'build'
                         //values "build-1","build-2","build-3","build-4","build-5","build-6"
-                        values "build-1","build-2"
+                        values "build-1", "build-2"
                     }
                     axis {
                         name 'localrepo'
-                        values "build-A","build-B","build-C","build-D","build-E","build-F"
+                        values "build-A", "build-B", "build-C", "build-D", "build-E", "build-F"
                     }
                 }
                 stages {
                     stage('build') {
                         steps {
-                            dir ("${build}_${localRepo}") {
+                            dir("${build}_${localRepo}") {
                                 git gitMavenRepo
                                 //This might break the builds in other threads
                                 //sh "rm -Rfv ${pvcMountCacheDir}/*"
@@ -63,7 +63,7 @@ pipeline {
                 }
             }
         }
-        stage ("show-cache"){
+        stage("show-cache") {
             steps {
                 sh "ls -ltRa ${pvcMountCacheDir}/de"
             }
